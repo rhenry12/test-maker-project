1e21d3d3e84d11d32977dc7c000b914ec0f1c169
rhenry12 <rohan.k.henry@gmail.com>
2014-11-02 17:13:13 -0500

    Committing initial files for project

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php include("inc/header.html");?>
+<br>
+<br>
+<div class="container">
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="login-panel panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Contact</h3>	
+				</div>
+				<div class="panel-body">
+					<form name="enq" method="post" action="email/" onsubmit="return validation();">
+					  <fieldset>
+					  	<div class="form-group">
+							<input type="text" name="name" id="name" value=""  class="form-control" placeholder="Name" />
+						</div>
+						<div class="form-group">
+					    	<input type="text" name="email" id="email" value="" class="form-control" placeholder="Email" />
+					    </div>
+					    <div class="form-group">
+					    	<label>Subject: </label>
+					    		<label class="radio-inline">
+					    		<input type="radio" name="optionsradioinline" id="optionsradioinline1" value="option1">
+					    		option
+				    			</label>
+
+					    		<label class="radio-inline">
+					    		<input type="radio" name="optionsradioinline" id="optionsradioinline2" value="option2">
+					    		option2
+				    			</label>
+
+					    		<label class="radio-inline">
+					    		<input type="radio" name="optionsradioinline" id="optionsradioinline3" value="option3">
+					    		option3
+				    			</label>
+					    </div>
+					    <div class="form-group">
+					    	<textarea rows="11" name="message" id="message" class="form-control" placeholder="Comments"></textarea>
+						</div>
+						<div class="form-group">
+							<input type="submit" value="Send!" name="submit" id="submitButton" class="btn btn-default btn-lg" title="Send!" />
+						</div>		  
+					  </fieldset>
+					</form>
+				</div>  
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php include("inc/footer.html");?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php include("inc/header.html");?>
+
+    <!-- Image Background Page Header -->
+    <!-- Note: The background image is set within the business-casual.css file. -->
+    <header class="business-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="tagline">Journey - Profesional Teaching tools for all</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-8">
+                <h2>What We Do</h2>
+                <p>Introduce the visitor to the business using clear, informative text. Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et molestiae similique eligendi reiciendis sunt distinctio odit? Quia, neque, ipsa, adipisci quisquam ullam deserunt accusantium illo iste exercitationem nemo voluptates asperiores.</p>
+                <p>
+                    <a class="btn btn-default btn-lg" href="#">Take a tour &raquo;</a>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h2>Contact Us</h2>
+                <address>
+                    <strong>Start Bootstrap</strong>
+                    <br>3481 Melrose Place
+                    <br>Beverly Hills, CA 90210
+                    <br>
+                </address>
+                <address>
+                    <abbr title="Phone">P:</abbr>(123) 456-7890
+                    <br>
+                    <abbr title="Email">E:</abbr> <a href="mailto:#">name@example.com</a>
+                </address>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <h2>Create</h2>
+                <p>Create a test online with ease. Use the simple create a test wizard to create test questions, 
+                    answers, and optional hints. The wizard will walkyou through the entire process.</p>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <h2>Publish</h2>
+                <p>Posting your test easy. Our site allows your test takers to create profiles and access the exams all from our secure site.</p>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <h2>Grade</h2>
+                <p>Grading is automatic. Once the test is complete, the score will be available once blah blah blah.</p>
+            </div>
+        </div>
+    <div>
+        <!-- /.row -->
+
+<?php include("inc/footer.html");?>
